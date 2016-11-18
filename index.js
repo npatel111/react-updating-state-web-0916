fef09bac67b812e3f7c4e9d02c49c8117d6da476
Niti Nrw <niti.patel@flatironschool.com>
2016-11-17 20:06:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much.";
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      hasBeenClicked: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // Update our state here...
+    this.setState({
+      hasBeenClicked: true,
+    })
+    console.log(this.state.hasBeenClicked);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hasBeenClicked ? 'not' : null} been clicked yet!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
